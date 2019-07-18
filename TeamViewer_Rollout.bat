a219b77f23b7b5f6fc5655adae6a476e02144a84
TurnkeyProcessing <53020025+TurnkeyProcessing@users.noreply.github.com>
2019-07-18 08:32:54 -0500

    Uploaded Rollout file

---
+++ TeamViewer_Rollout.bat
@@ -0,0 +1,2 @@
+//Deploy MSI Package and Assign to Host
+msiexec.exe /i "\\10.61.2.16\it\TeamViewer Installer\TeamViewer_Host.msi" /qn CUSTOMCONFIGID=624vxcr APITOKEN="6145775-bbrUVxGM86q8MmrYxm5E" ASSIGNMENTOPTIONS="--grant-easy-access" 
